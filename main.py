644192220b9f53a7ddc812b7b24818a90b088d9d
Tomi09282 <112693387+Tomi09282@users.noreply.github.com>
2022-09-04 21:54:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-a =[]
-while len(a) < 7:
-    inp = int(input(f"Adj meg egy számot({len(a)+1}/7): "))
-    a.append(inp)
+a = []
+while True:
+    inp = input(f"Adj meg egy számot({len(a)+1}): ")
+    if inp == "":
+        break
+    else:
+        a.append(int(inp))
 
-for i in range (1,7):
+for i in range (1,len(a)):
     j = i-1
     while j >= 0 and a[j] > a[j+1]:
         a[j], a[j+1] = a[j+1], a[j]
